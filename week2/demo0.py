5db90c756f7c9c398f635f723feab5f7a71b1e09
JinglanLiang10 <jinglanliang10@gmail.com>
2025-09-11 17:40:52 -0600

    first commit

---
+++ week2/demo0.py
@@ -0,0 +1,17 @@
+x = 3 
+y = 3.1
+coolMessage = "hewo!!!!"
+name = "Blue"
+maybe = True
+no = False
+print(x)
+print(coolMessage)
+print(name)
+print(y)
+print(True)
+print(no)
+# funny funny little comment
+x = 3
+c = x = 2
+z = 2 + c
+print(z)
---
+++ week2/firstDemo.py
@@ -0,0 +1,2 @@
+x = 3 # assigning x to 3, whenever x is called, it's also calling the value "3" so x is 3
+print(x)
