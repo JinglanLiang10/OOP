ceb72e90affc0ac05bcd69f3c3dc54277458903d
JinglanLiang10 <jinglanliang10@gmail.com>
2025-09-16 12:45:33 -0600

    Week 3

---
+++ week3/circle.py
@@ -0,0 +1,11 @@
+# I want to compute the circum and the area of the circle
+# assume the circle radius is 5
+# and that pi = 3.14
+
+radius = 5
+pI = 3.14
+circumference = radius*2*pI
+area = pI*(radius**2)
+print(circumference)
+print(area)
+
---
+++ week3/demo2.py
@@ -0,0 +1,25 @@
+x = 5 # assignment statement 
+print(x) # function call
+
+# variable
+# variable have a name
+#, has a value
+#, has an address
+#, has a size
+#, and has a type
+from typing import Final
+w: Final = 1
+w = 2
+i = 3 # int 
+f = 4.5 # flaot
+b = True # boolean
+s = "Hello world" # string
+input("you silly silly little ")
+print(type(i))
+print('-------------------------')
+x = 4.3 # x change class type from int to float
+print(x)
+x = "goo goo ga ga " # changed to string 
+print(x)
+x = True # changed to boolean
+print(x)
